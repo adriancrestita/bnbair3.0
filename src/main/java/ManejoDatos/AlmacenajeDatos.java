a7b64aab7fb4edf78ac0ff1a575ca74bf8ec15f1
adrian.crestita <adrian.morales@edu.uah.es>
2024-05-12 19:36:20 +0200

    comeonbro

--- src/main/java/ManejoDatos/AlmacenajeDatos.java
+++ src/main/java/ManejoDatos/AlmacenajeDatos.java
@@ -1,6 +1,8 @@
 package ManejoDatos;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import poo.javabnb.ClienteParticular;
 import poo.javabnb.TarjetaCredito;
 
@@ -8,7 +10,7 @@ public class AlmacenajeDatos {
     
     public static void guardarParticular(String dni, String nombre, String correo, String contraseña, String telefono, boolean VIP, String titular, String numtarj, String fcad) { //este metodo de guardado es utilizando la serialización de objetos
         ClienteParticular cliente = new ClienteParticular(dni, nombre, correo, contraseña, telefono, VIP);
-        TarjetaCredito datosBancarios = new TarjetaCredito(titular, numtarj, fcad);
+        TarjetaCredito datosBancarios = new TarjetaCredito(correo, titular, numtarj, fcad);
 
         
         Serializador serializador = new Serializador();
@@ -17,13 +19,12 @@ public class AlmacenajeDatos {
         try {
             serializador.guardarCliente(cliente, "cliente.dat");
             serializador.guardarDatosBancarios(datosBancarios, "datosBancarios.dat");
-
-            ClienteParticular clienteLeido = (ClienteParticular) deserializador.leerCliente("cliente.dat");
-            TarjetaCredito datosBancariosLeidos = (TarjetaCredito) deserializador.leerDatosBancarios    ("datosBancarios.dat");
-
+            
             // Utilizar clienteLeido y datosBancariosLeidos como necesites
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }   
+    
+    
 }
